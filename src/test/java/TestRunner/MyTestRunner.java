0d31f82babdb0d11aee6d201aba4255dfb2223ff
riyaz <riyazshaik219@gmail.com>
2021-10-04 21:06:27 +0530

    hybrid framework with junit

---
+++ src/test/java/TestRunner/MyTestRunner.java
@@ -0,0 +1,18 @@
+package TestRunner;
+
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class )
+@CucumberOptions(
+        features={"src/test/resources/features"},
+        glue = {"stepDefinitions","AppHooks"},
+        dryRun = false,
+        plugin = {"pretty","com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
+                "timeline:test-output-thread/"}
+
+)
+public class MyTestRunner {
+}
---
+++ test-output-thread/report.js
@@ -0,0 +1,87 @@
+$(document).ready(function() {
+CucumberHTML.timelineItems.pushArray([
+  {
+    "id": "449554ee-e860-4abb-8977-96fa68c2477f",
+    "feature": "Login Page feature",
+    "scenario": "Login Page title",
+    "start": 1633361114816,
+    "group": 1,
+    "content": "",
+    "tags": "",
+    "end": 1633361139920,
+    "className": "passed"
+  },
+  {
+    "id": "efa6ebe6-1c43-46f5-bf36-b0dfc4b69b03",
+    "feature": "Accounts Page feature",
+    "scenario": "Accounts Page section",
+    "start": 1633361083908,
+    "group": 1,
+    "content": "",
+    "tags": "",
+    "end": 1633361114766,
+    "className": "passed"
+  },
+  {
+    "id": "70201c11-7595-45f2-a13c-93e9e4455fea",
+    "feature": "Accounts Page feature",
+    "scenario": "Accounts Page section",
+    "start": 1633361005468,
+    "group": 1,
+    "content": "",
+    "tags": "",
+    "end": 1633361049891,
+    "className": "passed"
+  },
+  {
+    "id": "71e7d3d1-dee3-44f5-b3fd-70d9e520d5b8",
+    "feature": "Login Page feature",
+    "scenario": "Login with correct credentials",
+    "start": 1633361164063,
+    "group": 1,
+    "content": "",
+    "tags": "",
+    "end": 1633361204957,
+    "className": "failed"
+  },
+  {
+    "id": "02d260f8-a1d2-4107-993c-5955d1b496f5",
+    "feature": "Login Page feature",
+    "scenario": "Forgot Password Link",
+    "start": 1633361140002,
+    "group": 1,
+    "content": "",
+    "tags": "",
+    "end": 1633361164011,
+    "className": "passed"
+  },
+  {
+    "id": "2b99cea6-cefc-43d0-9c4c-fc3db4fa95ca",
+    "feature": "Accounts Page feature",
+    "scenario": "Accounts Page Title",
+    "start": 1633360958305,
+    "group": 1,
+    "content": "",
+    "tags": "",
+    "end": 1633361005359,
+    "className": "passed"
+  },
+  {
+    "id": "55d42e22-5389-452d-904e-761cfcbc6506",
+    "feature": "Accounts Page feature",
+    "scenario": "Accounts Page Title",
+    "start": 1633361050001,
+    "group": 1,
+    "content": "",
+    "tags": "",
+    "end": 1633361083819,
+    "className": "passed"
+  }
+]);
+CucumberHTML.timelineGroups.pushArray([
+  {
+    "id": 1,
+    "content": "Thread[main,5,main]"
+  }
+]);
+});
